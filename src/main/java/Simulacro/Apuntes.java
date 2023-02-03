5ce19eaa182c185b025268e6a626caf684f76d5c
Pablodaw2022 <pnavgarb@g.educaand.es>
2023-02-03 08:47:12 +0100

    Repaso ejercicios

---
+++ src/main/java/Simulacro/Apuntes.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Simulacro;
+
+/**
+ *
+ * @author samue
+ */
+public class Apuntes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        int row = 5;
+//int col = 5;
+//int[][] matrix = new int[row][col];
+//
+//matrix[0][0] = 2000;
+//matrix[0][col-1] = 2004;
+//matrix[row-1][0] = 2008;
+//matrix[row-1][col-1] = 2012;
+//
+//for (int i = 1; i < row; i++) {
+//   for (int j = 1; j < col; j++) {
+//      matrix[i][j] = matrix[i-1][j-1] + 365;
+//    }
+//    
+//   
+//   public static String convertDate(int year) {
+//   SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//   Date date = new Date(year, 0, 1);
+//   return sdf.format(date);
+//}
+//   
+//   
+//   for (int i = 0; i < row; i++) {
+//   for (int j = 0; j < col; j++) {
+//      System.out.println(convertDate(matrix[i][j]));
+//   }
+//}
+
+/*
+Definir la matriz con un tamaño determinado y las fechas en formato chino.
+Rellenar las esquinas de la matriz con los años bisiestos.
+Recorrer la matriz y llenar las diagonales con la suma de los días de la casilla anterior.
+Para convertir las fechas de formato chino a peruano, se puede usar una función que realice la conversión.
+Una vez convertidas las fechas, se pueden obtener las vecinas en formato peruano.*/
+}
